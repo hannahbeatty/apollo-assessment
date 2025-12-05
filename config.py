4e9471f93827bf181a14b9e6227e61f0da68ae69
Hannah Beatty <hannahrbeatty@gmail.com>
2025-12-04 22:04:55 -0500

    created app folder

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'vehicles.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
