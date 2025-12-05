345de778b76ac4b3588571d9fb791d933319301a
Hannah Beatty <hannahrbeatty@gmail.com>
2025-12-04 22:24:48 -0500

    added tester config

--- app/config.py
+++ app/config.py
@@ -4,3 +4,9 @@ class Config:
     BASEDIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'instance', 'vehicles.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TESTING = False
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"   # or replaced by pytest later
